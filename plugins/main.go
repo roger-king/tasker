afa35de80845cedaf3865e3cf55ea3240dbd5d9d
Roger King <rogr.king@gmail.com>
2019-11-10 15:13:26 -0500

    fix: updating plugin example

--- plugins/main.go
+++ plugins/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+
+	"github.com/roger-king/tasker/utils"
 )
 
 type payload struct {
-	Name string `json:"name"`
+	Name string `json:"name" validate:"required"`
 }
 
 func Run(args map[string]interface{}) error {
 	var p payload
-	jsonString, err := json.Marshal(args)
+
+	err := utils.Validate(args, p)
 
 	if err != nil {
 		return err
 	}
-
-	json.Unmarshal([]byte(jsonString), &p)
 
 	fmt.Println("Hello Tasker: ", p.Name)
 	return nil
