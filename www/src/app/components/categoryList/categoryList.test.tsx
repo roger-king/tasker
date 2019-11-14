3e7fd14819bed42ae77cad8e7c0e3879d38f5086
Roger King <rogr.king@gmail.com>
2019-11-13 19:25:13 -0500

    first snapshot test

--- www/src/app/components/categoryList/categoryList.test.tsx
+++ www/src/app/components/categoryList/categoryList.test.tsx
@@ -3,14 +3,33 @@ import renderer from 'react-test-renderer';
 import CategoryList from './index';
 
 describe('<CategoryList />', () => {
-    it('Matches the snapshot', () => {
+    const categories = ['All', 'main', 'create'];
+    let currentCategory: string | null = null;
+    it('Matches the snapshot with ALL selected', () => {
         const select = jest.fn();
         const openModal = jest.fn();
         const list = renderer
             .create(
                 <CategoryList
-                    categories={['All', 'main', 'create']}
-                    current={null}
+                    categories={categories}
+                    current={currentCategory}
+                    selectCategory={select}
+                    openModal={openModal}
+                />,
+            )
+            .toJSON();
+        expect(list).toMatchSnapshot();
+    });
+
+    it('Matches the snapshot with MAIN selected', () => {
+        currentCategory = categories[1];
+        const select = jest.fn();
+        const openModal = jest.fn();
+        const list = renderer
+            .create(
+                <CategoryList
+                    categories={categories}
+                    current={currentCategory}
                     selectCategory={select}
                     openModal={openModal}
                 />,
