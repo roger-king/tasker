5735704812b998ca74ce884480663466903e1434
Roger King <rogr.king@gmail.com>
2019-11-16 11:07:27 -0500

    feat: adding more typescript helpers

--- www/src/app/components/prettyJSON/index.tsx
+++ www/src/app/components/prettyJSON/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Box } from 'grommet';
+import styled from 'styled-components';
 
 interface PrettyJSONProps {
+    className?: string;
     data: Record<string, any>;
+    spaces?: number;
 }
 
 const PrettyJSON: React.FC<PrettyJSONProps> = (props: PrettyJSONProps) => {
-    const { data } = props;
+    const { className, data, spaces = 4 } = props;
 
     return (
-        <Box margin="small" pad="medium" background="light-4" align="center" justify="center">
-            <pre>{JSON.stringify(data)}</pre>
+        <Box className={className} margin="small" pad="medium" background="light-4" align="center" justify="center">
+            <pre>{JSON.stringify(data, null, spaces)}</pre>
         </Box>
     );
 };
 
-export default React.memo(PrettyJSON);
+export default styled(React.memo(PrettyJSON))`
+    min-width: 400px;
+    max-width: 700px;
+`;
--- www/src/types/index.d.ts
+++ www/src/types/index.d.ts
@@ -1,3 +1,6 @@
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+type Optionalize<T extends K, K> = Omit<T, keyof K>;
+
 interface Task {
     taskId: string;
     entryId: number;
