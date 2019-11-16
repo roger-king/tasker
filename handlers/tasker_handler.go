8a4a172a32959e25db95d87dd8cc88d560aa0781
Roger King <rogr.king@gmail.com>
2019-11-16 10:16:32 -0500

    fix: not handling empty task id

--- handlers/tasker_handler.go
+++ handlers/tasker_handler.go
@@ -2,9 +2,8 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
 
 	"github.com/gorilla/mux"
 	"github.com/roger-king/tasker/models"
@@ -54,7 +53,14 @@ func CreateTask(t *services.TaskService) http.HandlerFunc {
 func FindTask(t *services.TaskService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		task, err := t.Find(vars["taskID"])
+		taskId := vars["taskID"]
+
+		if len(taskId) == 0 {
+			respondWithError(w, http.StatusInternalServerError, utils.ProcessingError, errors.New("no task id was supplied").Error())
+			return
+		}
+
+		task, err := t.Find(taskId)
 
 		if err != nil {
 			respondWithError(w, http.StatusInternalServerError, utils.ProcessingError, err.Error())
@@ -70,7 +76,14 @@ func FindTask(t *services.TaskService) http.HandlerFunc {
 func DisableTask(t *services.TaskService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		err := t.Disable(vars["taskID"])
+		taskId := vars["taskID"]
+
+		if len(taskId) == 0 {
+			respondWithError(w, http.StatusInternalServerError, utils.ProcessingError, errors.New("no task id was supplied").Error())
+			return
+		}
+
+		err := t.Disable(taskId)
 
 		if err != nil {
 			respondWithError(w, http.StatusInternalServerError, utils.ProcessingError, err.Error())
@@ -86,7 +99,14 @@ func DisableTask(t *services.TaskService) http.HandlerFunc {
 func DeleteTask(t *services.TaskService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		err := t.Delete(vars["taskID"])
+		taskId := vars["taskID"]
+
+		if len(taskId) == 0 {
+			respondWithError(w, http.StatusInternalServerError, utils.ProcessingError, errors.New("no task id was supplied").Error())
+			return
+		}
+
+		err := t.Delete(taskId)
 
 		if err != nil {
 			respondWithError(w, http.StatusInternalServerError, utils.ProcessingError, err.Error())
@@ -96,24 +116,4 @@ func DeleteTask(t *services.TaskService) http.HandlerFunc {
 		respondWithJSON(w, http.StatusOK, true)
 		return
 	}
-}
-
-// ServeWebAdmin -
-func ServeWebAdmin(w http.ResponseWriter, r *http.Request) {
-
-	if len(utils.TaskerEnv) == 0 || utils.TaskerEnv == "local" {
-		url, _ := url.Parse("http://localhost:3000")
-		// create the reverse proxy
-		proxy := httputil.NewSingleHostReverseProxy(url)
-
-		// Update the headers to allow for SSL redirection
-		r.URL.Host = url.Host
-		r.URL.Scheme = url.Scheme
-		r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
-		r.Host = url.Host
-
-		// Note that ServeHttp is non blocking and uses a go routine under the hood
-		proxy.ServeHTTP(w, r)
-	}
-	return
 }
