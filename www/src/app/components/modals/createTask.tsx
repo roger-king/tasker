cb0a9f7d641147cf47420b86100b2739811dc314
Roger King <rogr.king@gmail.com>
2019-11-17 21:13:52 -0500

    wip: create form map

--- www/src/app/components/modals/createTask.tsx
+++ www/src/app/components/modals/createTask.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Button, Form, FormField, Heading, Layer } from 'grommet';
+import { Box, Button, FormField, Heading, Layer, TextInput } from 'grommet';
 import { Close, Add } from 'grommet-icons';
 
 const ArgsField: React.FC<{}> = (): JSX.Element => {
@@ -42,31 +42,56 @@ interface CreateTaskModalProps {
 const CreateTaskModal: React.FC<CreateTaskModalProps> = (props: CreateTaskModalProps): JSX.Element => {
     const { showModal } = props;
     const [next, setNext] = useState<boolean>(false);
+    const [createTaskInput, setCreateTaskInput] = useState<Map<string, string | Record<string, any>>>(new Map());
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const [disableNext, _setDisableNext] = useState<boolean>(true);
+
+    const onChange = (e: any): void => {
+        const key = e.target.name;
+        const value = e.target.value;
+
+        setCreateTaskInput(createTaskInput.set(key, value));
+
+        console.log(createTaskInput.size);
+    };
 
     return (
         <Layer modal onClickOutside={(): void => showModal()} onEsc={(): void => showModal()}>
-            <Box width="medium" pad="medium">
+            <Box width="large" pad="medium">
                 <Box direction="row">
                     <Button icon={<Close size="medium" />} onClick={(): void => showModal()} />
                     <Heading level="4">Create Task</Heading>
                 </Box>
-                <Form>
+                <Box>
                     {!next ? (
                         <Box>
-                            <FormField label="Name" />
-                            <FormField label="Description" />
-                            <FormField label="Schedule" />
-                            <FormField label="Executor" />
-                            <Button label="Next" onClick={(): void => setNext(true)} />
+                            <FormField label="Name">
+                                <TextInput name="name" onChange={onChange} required />
+                            </FormField>
+                            <FormField label="Description">
+                                <TextInput name="description" onChange={onChange} required />
+                            </FormField>
+                            <FormField label="Schedule">
+                                <TextInput name="schedule" onChange={onChange} required />
+                            </FormField>
+                            <FormField label="Executor">
+                                <TextInput name="executor" onChange={onChange} required />
+                            </FormField>
+                            <Button
+                                label="Next"
+                                onClick={(): void => setNext(true)}
+                                style={{ borderRadius: '7px' }}
+                                disabled={disableNext}
+                            />
                         </Box>
                     ) : (
-                        <Box>
+                        <Box fill>
                             <Heading level="4">Args: </Heading>
                             <ArgsFieldList />
                             <Button label="Create" />
                         </Box>
                     )}
-                </Form>
+                </Box>
             </Box>
         </Layer>
     );
