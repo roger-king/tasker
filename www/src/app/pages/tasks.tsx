db48c718aaa8c74288f6e9284d8ab269062ee019
Roger King <rogr.king@gmail.com>
2019-11-22 06:41:33 -0500

    feat: handle 0 tasks

--- www/src/app/pages/tasks.tsx
+++ www/src/app/pages/tasks.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Select } from 'grommet';
+import { Box, Select, Heading, Text } from 'grommet';
+import { Tasks } from 'grommet-icons';
 import styled from 'styled-components';
 
 import CategoryList from '../components/categoryList';
@@ -18,6 +19,7 @@ interface HomePageState {
     tasks: Task[];
     filteredTasks: Task[];
     showModal: boolean;
+    isLoading: boolean;
 }
 
 class HomePage extends React.PureComponent<HomePageProps, HomePageState> {
@@ -30,6 +32,7 @@ class HomePage extends React.PureComponent<HomePageProps, HomePageState> {
             tasks: [],
             filteredTasks: [],
             showModal: false,
+            isLoading: true,
         };
     }
 
@@ -37,15 +40,18 @@ class HomePage extends React.PureComponent<HomePageProps, HomePageState> {
         const tasks = await listTasks();
         const categories: string[] = [];
 
-        tasks.data.map((t: Task): void => {
-            if (categories.indexOf(t.executor) === -1) {
-                categories.push(t.executor);
-            }
-            // eslint-disable-next-line
-            return;
-        });
+        if (tasks.data && tasks.data.length > 0) {
+            tasks.data.map((t: Task): void => {
+                if (categories.indexOf(t.executor) === -1) {
+                    categories.push(t.executor);
+                }
+                // eslint-disable-next-line
+                return;
+            });
+            this.setState({ categories, tasks: tasks.data });
+        }
 
-        this.setState({ categories, tasks: tasks.data });
+        this.setState({ isLoading: false });
     }
 
     setCurrentCategory = (c: string | null): void => {
@@ -66,9 +72,32 @@ class HomePage extends React.PureComponent<HomePageProps, HomePageState> {
         this.setState({ showModal: !showModal });
     };
 
+    renderBody = () => {
+        const { currentCategory, categories, tasks, filteredTasks, isLoading } = this.state;
+        if (isLoading) {
+            return <Box>loading...</Box>;
+        }
+        if (tasks.length === 0) {
+            return (
+                <Box align="center" justify="center">
+                    <Tasks size="large" />
+                    <Heading level="3">No tasks set.</Heading>
+                    <Text>To create a task click the big blue button on the left.</Text>
+                </Box>
+            );
+        }
+        return (
+            <TaskList
+                tasks={filteredTasks.length === 0 ? tasks : filteredTasks}
+                categories={categories}
+                currentCategory={currentCategory}
+            />
+        );
+    };
+
     render(): JSX.Element {
         const { className } = this.props;
-        const { currentCategory, categories, tasks, filteredTasks, showModal } = this.state;
+        const { currentCategory, categories, showModal } = this.state;
         return (
             <Box className={className} direction="row" gap="large" justify="between" fill pad="xlarge">
                 <CategoryList
@@ -78,15 +107,7 @@ class HomePage extends React.PureComponent<HomePageProps, HomePageState> {
                     openModal={this.setShowModal}
                 />
                 <Box align="center" pad={{ left: '70px' }} width="100%">
-                    {tasks.length === 0 ? (
-                        'loading...'
-                    ) : (
-                        <TaskList
-                            tasks={filteredTasks.length === 0 ? tasks : filteredTasks}
-                            categories={categories}
-                            currentCategory={currentCategory}
-                        />
-                    )}
+                    {this.renderBody()}
                 </Box>
                 <Box direction="row" align="start" justify="center" gap="small">
                     <Select options={['Last 7 days']} size="small" value="Last 7 days" />
