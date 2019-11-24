e8d8e2407d12a884fc0a29324d387cec91d70bdb
Roger King <rogr.king@gmail.com>
2019-11-23 20:25:19 -0500

    feat: create array with leading zero

--- www/src/app/utils/array.ts
+++ www/src/app/utils/array.ts
@@ -1,5 +1,12 @@
 export const range = (start: number, end: number) => {
     return Array(end - start + 1)
         .fill(undefined)
-        .map((_: number, idx: number) => start + idx);
+        .map((_: number, idx: number) => {
+            const num = String(start + idx);
+            if (num.length === 1) {
+                return `0${num}`;
+            }
+
+            return num;
+        });
 };
