897f8b120f337a83e25fb08408bac0cde8ead6aa
Roger King <rogr.king@gmail.com>
2019-11-24 22:28:06 -0500

    wip: settings page

--- www/src/app/pages/settings/index.tsx
+++ www/src/app/pages/settings/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Heading, Button } from 'grommet';
 import { Route, Switch, RouteComponentProps, Redirect } from 'react-router-dom';
 import PluginSettingsPage from './plugin';
@@ -6,20 +6,30 @@ import PluginSettingsPage from './plugin';
 const SettingsPage: React.FC<RouteComponentProps> = (props: RouteComponentProps): JSX.Element => {
     const { match, history } = props;
     const { url } = match;
+    const [settingHeader, setSettingHeader] = useState<string>('Settings');
 
     return (
-        <Box margin="xlarge">
+        <Box margin={{ left: '100px', right: '100px' }}>
             <Box margin={{ left: '200px' }}>
-                <Heading> Settings </Heading>
+                <Heading> {settingHeader} </Heading>
             </Box>
-            <Box direction="row" height="100%">
+            <Box direction="row" height="100%" margin={{ top: '40px' }}>
                 <Box direction="column" width="150px" border={{ side: 'right', size: '3px' }} pad="medium">
                     <Button label="Plugin" plain onClick={() => history.push(`${url}/plugin`)} />
                 </Box>
-                <Switch>
-                    <Route exact path={`${url}/plugin`} component={PluginSettingsPage} />
-                    <Redirect from={`${url}`} to={`${url}/plugin`} />
-                </Switch>
+                <Box margin={{ left: '50px' }}>
+                    <Switch>
+                        <Route
+                            exact
+                            path={`${url}/plugin`}
+                            render={() => {
+                                setSettingHeader('Plugin Settings');
+                                return <PluginSettingsPage />;
+                            }}
+                        />
+                        <Redirect from={`${url}`} to={`${url}/plugin`} />
+                    </Switch>
+                </Box>
             </Box>
         </Box>
     );
