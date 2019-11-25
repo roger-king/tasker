61eeedec5b6515746463011111ce3d7d7a2787a1
Roger King <rogr.king@gmail.com>
2019-11-24 20:01:27 -0500

    fix: settings page import

--- www/src/app/pages/index.ts
+++ www/src/app/pages/index.ts
@@ -3,5 +3,5 @@ import React from 'react';
 export const OverviewPage = React.lazy(() => import('./overview'));
 export const TasksPage = React.lazy(() => import('./tasks'));
 export const TaskPage = React.lazy(() => import('./task'));
-export const SettingsPage = React.lazy(() => import('./settings'));
+export const SettingsPage = React.lazy(() => import('./settings/index'));
 export const LoginPage = React.lazy(() => import('./login'));
