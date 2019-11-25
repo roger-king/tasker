fbfe6e935698f30fd9759ca2265481e9027b79f2
Roger King <rogr.king@gmail.com>
2019-11-24 22:27:55 -0500

    wip: plugin settings page

--- www/src/app/pages/settings/plugin.tsx
+++ www/src/app/pages/settings/plugin.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Heading } from 'grommet';
+import { listSettings } from '../../data/settings';
 
 const PluginSettingPage: React.FC = () => {
-    return (
-        <Box>
-            <Heading>plugin</Heading>
-        </Box>
-    );
+    const [loading, setLoading] = useState<boolean>(true);
+    const [settings, setSettings] = useState<Setting[] | null>(null);
+    const [error, setError] = useState<any>();
+
+    useEffect(() => {
+        listSettings()
+            .then(({ data }: { data: Setting[] | null }) => {
+                setLoading(false);
+                setSettings(data);
+            })
+            .catch((err: any) => {
+                setError(err);
+            });
+    }, []);
+
+    if (error) {
+        return <Box>{error}</Box>;
+    }
+
+    if (loading) {
+        return <Box>loading...</Box>;
+    }
+
+    if (settings && settings.length > 0) {
+        return (
+            <Box>
+                <Heading level="4">Repositories:</Heading>
+                <Box>
+                    {settings.map((s: Setting) => {
+                        return <pre key={`${s.repo_name}-code`}>{s.repo_name}</pre>;
+                    })}
+                </Box>
+            </Box>
+        );
+    }
+
+    return <Box>configure your plugin</Box>;
 };
 
 export default PluginSettingPage;
