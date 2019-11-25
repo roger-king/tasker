8e4eec9c4a3a21cf9645383e700fb74c0a1d7808
Roger King <rogr.king@gmail.com>
2019-11-24 20:01:40 -0500

    wip: settings page

--- www/src/app/pages/settings/index.tsx
+++ www/src/app/pages/settings/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Box, Heading } from 'grommet';
+import { Box, Heading, Button } from 'grommet';
+import { Route, Switch, RouteComponentProps } from 'react-router-dom';
+import PluginSettingsPage from './plugin';
 
-const SettingsPage: React.FC<{}> = (): JSX.Element => {
+const SettingsPage: React.FC<RouteComponentProps> = (props: RouteComponentProps): JSX.Element => {
+    const { match, history } = props;
+    const { url } = match;
+
     return (
-        <Box margin={{ left: '300px', right: '300px' }}>
-            <Heading> Settings </Heading>
+        <Box margin="xlarge">
+            <Box margin={{ left: '200px' }}>
+                <Heading> Settings </Heading>
+            </Box>
+            <Box direction="row" height="100%">
+                <Box direction="column" width="150px" border={{ side: 'right', size: '3px' }} pad="medium">
+                    <Button label="Plugin" plain onClick={() => history.push(`${url}/plugin`)} />
+                </Box>
+                <Switch>
+                    <Route exact path={`${url}/plugin`} component={PluginSettingsPage} />
+                </Switch>
+            </Box>
         </Box>
     );
 };
---
+++ www/src/app/pages/settings/plugin.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Heading } from 'grommet';
+
+const PluginSettingPage: React.FC = () => {
+    return (
+        <Box>
+            <Heading>plugin</Heading>
+        </Box>
+    );
+};
+
+export default PluginSettingPage;
