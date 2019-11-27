dd5f56447a5bd2ec5c83680d5683b0ce790dfbe1
Roger King <rogr.king@gmail.com>
2019-11-27 12:42:33 -0500

    feat: base modal component

---
+++ www/src/app/components/modals/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Box, Layer, Button, Heading } from 'grommet';
+import { Close } from 'grommet-icons';
+
+interface ModalProps {
+    setShowModal: any;
+    header: string;
+    width: any;
+    onClickOutside: boolean;
+    onEsc: boolean;
+}
+
+class Modal extends React.PureComponent<ModalProps, {}> {
+    render(): JSX.Element {
+        const { setShowModal, header, children, width, onClickOutside, onEsc } = this.props;
+
+        return (
+            <Layer
+                modal
+                onClickOutside={(): void => {
+                    if (onClickOutside) {
+                        setShowModal(false);
+                    }
+                }}
+                onEsc={(): void => {
+                    if (onEsc) {
+                        setShowModal(false);
+                    }
+                }}
+            >
+                <Box width={width} pad="medium">
+                    <Box direction="row">
+                        <Button icon={<Close />} onClick={(): void => setShowModal(false)} />
+                        <Heading level="3"> {header}</Heading>
+                    </Box>
+                    <Box margin="small">{children}</Box>
+                </Box>
+            </Layer>
+        );
+    }
+}
+
+export default Modal;
